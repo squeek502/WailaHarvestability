e981c8b46dcf52869f3fb333f4c99ee15f323139
squeek <squeek502@hotmail.com>
2014-08-25 17:56:06 -0700

    Fixed effective tool for blocks that use material-based tool effectiveness
     * For example, pickaxes are effective against all blocks with the Material.rock material

--- java/squeek/wailaharvestability/helpers/BlockHelper.java
+++ java/squeek/wailaharvestability/helpers/BlockHelper.java
@@ -1,11 +1,24 @@
 package squeek.wailaharvestability.helpers;
 
+import java.util.HashMap;
 import squeek.wailaharvestability.proxy.ProxyIguanaTweaks;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 
 public class BlockHelper
 {
+	private static final HashMap<String, ItemStack> testTools = new HashMap<String, ItemStack>();
+	static
+	{
+		testTools.put("pickaxe", new ItemStack(Item.pickaxeWood));
+		testTools.put("shovel", new ItemStack(Item.shovelWood));
+		testTools.put("axe", new ItemStack(Item.axeWood));
+	}
 
 	public static boolean getHarvestLevelsOf(Block block, int metadata, String[] toolClasses, int[] harvestLevels)
 	{
@@ -18,10 +31,35 @@ public class BlockHelper
 
 			if (harvestLevels[i] != -1)
 				hasEffectiveTools = true;
+			else
+			{
+				float hardness = 0f;
+				try
+				{
+					hardness = block.getBlockHardness(null, 0, 0, 0);
+				}
+				catch (Exception e)
+				{
+				}
+				if (hardness > 0f)
+				{
+					ItemStack testTool = testTools.get(toolClass);
+					if (testTool != null && testTool.getItem() instanceof ItemTool && testTool.getStrVsBlock(block) >= ((ItemTool) testTool.getItem()).efficiencyOnProperMaterial)
+					{
+						harvestLevels[i] = 0;
+						hasEffectiveTools = true;
+					}
+				}
+			}
 
 			i++;
 		}
 		return hasEffectiveTools;
+	}
+	
+	public static boolean isBlockUnbreakable(Block block, World world, int x, int y, int z)
+	{
+		return block.getBlockHardness(world, x, y, z) == -1.0f;
 	}
 
 }
