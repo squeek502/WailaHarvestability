a2f43f619692c44f3b27d72032118ae15d2f6a39
squeek <squeek502@hotmail.com>
2015-10-18 18:27:50 -0700

    Remove obsolete TiC backwards compatibility code  * Fixes hard-coded Atlarus localization  * Changes fallback localization to be Waila Harvestability specific (wailaharvestability.harvestlevel<num>)

--- java/squeek/wailaharvestability/helpers/StringHelper.java
+++ java/squeek/wailaharvestability/helpers/StringHelper.java
@@ -1,7 +1,6 @@
 package squeek.wailaharvestability.helpers;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
 import java.util.List;
 import squeek.wailaharvestability.ModWailaHarvestability;
 import squeek.wailaharvestability.proxy.ProxyIguanaTweaks;
@@ -25,7 +24,6 @@ public class StringHelper
 		}
 	}
 
-	// Taken from tconstruct.client.gui.ToolStationGui
 	public static String getHarvestLevelName(int num)
 	{
 		if (getHarvestLevelName != null)
@@ -38,48 +36,16 @@ public class StringHelper
 			{
 			}
 		}
-		else
-		{
-			if (ModWailaHarvestability.hasIguanaTweaks)
-				return ProxyIguanaTweaks.getHarvestLevelName(num);
 
-			String unlocalized = "gui.partcrafter.mining" + (num + 1);
-			String localized = StringHelper.getLocalizedString(unlocalized);
-			if (!unlocalized.equals(localized))
-				return localized;
-		}
+		if (ModWailaHarvestability.hasIguanaTweaks)
+			return ProxyIguanaTweaks.getHarvestLevelName(num);
+
+		String unlocalized = "wailaharvestability.harvestlevel" + (num + 1);
+
+		if (StatCollector.canTranslate(unlocalized))
+			return StatCollector.translateToLocal(unlocalized);
+
 		return String.valueOf(num);
-	}
-
-	// for TCon version < 1.5.3
-	public static HashMap<String, String> localizationAlternatives = new HashMap<String, String>();
-	static
-	{
-		localizationAlternatives.put("gui.partcrafter.mining1", "Stone");
-		localizationAlternatives.put("gui.partcrafter.mining2", "Iron");
-		localizationAlternatives.put("gui.partcrafter.mining3", "Redstone");
-		localizationAlternatives.put("gui.partcrafter.mining4", "Obsidian");
-		localizationAlternatives.put("gui.partcrafter.mining5", "Cobalt");
-		localizationAlternatives.put("gui.partcrafter.mining6", "Atlarus");
-		localizationAlternatives.put("gui.partcrafter.mining7", "7");
-		localizationAlternatives.put("gui.partcrafter.mining8", "8");
-		localizationAlternatives.put("gui.toolstation15", "Mining Level: ");
-	}
-
-	public static String getLocalizedString(String unlocalized)
-	{
-		if (unlocalized.equals("gui.partcrafter.mining6"))
-			return localizationAlternatives.get(unlocalized);
-
-		String localized = StatCollector.translateToLocal(unlocalized);
-		if (localized.equals(unlocalized))
-		{
-			localized = localizationAlternatives.get(unlocalized);
-
-			if (localized == null)
-				localized = unlocalized;
-		}
-		return localized;
 	}
 
 	public static String concatenateStringList(List<String> strings, String separator)
