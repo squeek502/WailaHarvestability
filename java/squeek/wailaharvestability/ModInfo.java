6622ce6b40f7d8a0d36d14052b20ab2b8b3ec62d
GirafiStudios <girafi0@gmail.com>
2016-06-27 06:25:03 +0200

    Un-lowercased the mod id

--- java/squeek/wailaharvestability/ModInfo.java
+++ java/squeek/wailaharvestability/ModInfo.java
@@ -2,6 +2,6 @@ package squeek.wailaharvestability;
 
 public final class ModInfo
 {
-	public static final String MODID = "wailaharvestability";
+	public static final String MODID = "WailaHarvestability";
 	public static final String VERSION = "${version}";
 }
