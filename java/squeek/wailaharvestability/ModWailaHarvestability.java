ef87cfe8a24cea0a5ca151674ac865be8ca1ce07
GirafiStudios <girafi0@gmail.com>
2016-07-01 06:40:06 +0200

    Made Waila Harvestability clientSideOnly, through the @Mod annoation

--- java/squeek/wailaharvestability/ModWailaHarvestability.java
+++ java/squeek/wailaharvestability/ModWailaHarvestability.java
@@ -7,16 +7,13 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import squeek.wailaharvestability.setup.MissingHarvestInfo;
 
-@Mod(modid = ModInfo.MODID, version = ModInfo.VERSION, acceptedMinecraftVersions="[1.10,1.11)", dependencies = "after:TConstruct;after:ExtraTiC;after:TSteelworks;after:Mariculture", acceptableRemoteVersions = "*", guiFactory = ModInfo.GUI_FACTORY_CLASS)
+@Mod(modid = ModInfo.MODID, version = ModInfo.VERSION, acceptedMinecraftVersions="[1.10,1.11)", dependencies = "after:TConstruct;after:ExtraTiC;after:TSteelworks;after:Mariculture", clientSideOnly = true, guiFactory = ModInfo.GUI_FACTORY_CLASS)
 public class ModWailaHarvestability
 {
 	public static boolean hasIguanaTweaks;
 
-	@SideOnly(Side.CLIENT)
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
@@ -26,7 +23,6 @@ public class ModWailaHarvestability
 		FMLInterModComms.sendMessage("VersionChecker", "addVersionCheck", "http://www.ryanliptak.com/minecraft/versionchecker/squeek502/WailaHarvestability");
 	}
 
-	@SideOnly(Side.CLIENT)
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
@@ -34,7 +30,6 @@ public class ModWailaHarvestability
 		FMLInterModComms.sendMessage("Waila", "register", "squeek.wailaharvestability.WailaHandler.callbackRegister");
 	}
 
-	@SideOnly(Side.CLIENT)
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event)
 	{
