cdefef104d4d8abf1c1083d8ee7a48f83b6ec62b
GirafiStudios <faj10@msn.com>
2020-04-30 23:57:35 +0200

    Ported to 1.15.2

--- java/squeek/wailaharvestability/helpers/KeyHelper.java
+++ java/squeek/wailaharvestability/helpers/KeyHelper.java
@@ -9,7 +9,7 @@ public class KeyHelper
 	public static boolean isCtrlKeyDown()
 	{
 		// prioritize CONTROL, but allow OPTION as well on Mac (note: GuiScreen's isCtrlKeyDown only checks for the OPTION key on Mac)
-		long handle = Minecraft.getInstance().mainWindow.getHandle();
+		long handle = Minecraft.getInstance().getMainWindow().getHandle();
 		boolean isCtrlKeyDown = InputMappings.isKeyDown(handle, GLFW.GLFW_KEY_LEFT_CONTROL) || InputMappings.isKeyDown(handle, GLFW.GLFW_KEY_RIGHT_CONTROL);
 		if (!isCtrlKeyDown && Minecraft.IS_RUNNING_ON_MAC)
 			isCtrlKeyDown = InputMappings.isKeyDown(handle, GLFW.GLFW_KEY_LEFT_ALT) || InputMappings.isKeyDown(handle, GLFW.GLFW_KEY_RIGHT_ALT);
@@ -19,7 +19,7 @@ public class KeyHelper
 
 	public static boolean isShiftKeyDown()
 	{
-		long handle = Minecraft.getInstance().mainWindow.getHandle();
+		long handle = Minecraft.getInstance().getMainWindow().getHandle();
 		return InputMappings.isKeyDown(handle, GLFW.GLFW_KEY_LEFT_SHIFT) || InputMappings.isKeyDown(handle, GLFW.GLFW_KEY_RIGHT_SHIFT);
 	}
 }
