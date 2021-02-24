5f4402e271bec4c2425dd5ed15d9f69f7747b0ca
LiskoSlayer63 <markus.ahola@hotmail.com>
2021-02-24 02:03:13 +0200

    Fixed compatibility error with servers (#79)

--- java/squeek/wailaharvestability/ModWailaHarvestability.java
+++ java/squeek/wailaharvestability/ModWailaHarvestability.java
@@ -1,12 +1,16 @@
 package squeek.wailaharvestability;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(value = ModInfo.MODID)
 public class ModWailaHarvestability
@@ -18,7 +22,8 @@ public class ModWailaHarvestability
 		final IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modBus.addListener(this::setupClient);
 		modBus.addListener(this::setupCommon);
-		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.spec);
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+	    ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.spec);
 	}
 
 	private void setupClient(FMLClientSetupEvent event)
